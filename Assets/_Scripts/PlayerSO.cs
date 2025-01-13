88c158113bfdd03c2f976ffeac2ffee14251cb54
Dan Hook <s188873@uos.ac.uk>
2025-01-13 13:06:29 +0000

    Added 'PlayerSO' scriptable object with events

---
+++ Assets/_Scripts/PlayerSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "PlayerSO", menuName = "ChainGame/PlayerSO")]
+public class PlayerSO : ScriptableObject
+{
+    public string Name;
+    public byte CurrentRoll;
+    public byte CurrentTilesMoved;
+    public bool CanSwap;
+
+    public event UnityAction<byte> OnCurrentRollChanged;
+    public event UnityAction<byte> OnCurrentTilesMovedChanged;
+    
+    public void CurrentRollChanged(byte roll) => OnCurrentRollChanged?.Invoke(roll);
+    public void CurrentTilesMovedChanged(byte tiles) => OnCurrentTilesMovedChanged?.Invoke(tiles);
+}
