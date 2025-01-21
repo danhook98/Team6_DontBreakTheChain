ab187b73241fe5e8984068a5a73529aacc9a12af
Dan Hook <s188873@uos.ac.uk>
2025-01-21 19:15:20 +0000

    Added audio listener script

---
+++ Assets/_Scripts/Audio/AudioListener.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioListener : MonoBehaviour
+{
+    [SerializeField] private AudioEventChannel audioEventChannel;
+    
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        audioEventChannel.OnPlayAudioEvent += PlayAudioOneShot;
+    }
+
+    private void OnDisable()
+    {
+        audioEventChannel.OnPlayAudioEvent -= PlayAudioOneShot;
+    }
+
+    private void PlayAudioOneShot(AudioClipSO audioClip)
+    {
+        _audioSource.PlayOneShot(audioClip.audioClip);
+    }
+}
