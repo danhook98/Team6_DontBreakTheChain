c735dc30ce2c7dc608df797d7373e0f5c358445b
Dan Hook <s188873@uos.ac.uk>
2025-02-09 18:48:00 +0000

    Added rotation and bobbing to coins

---
+++ Assets/_Scripts/CoinRotator.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CoinRotator : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 30f;
+    [SerializeField] private float bobSpeed = 1.5f;
+    [SerializeField] private float bobAmplitude = 0.25f;
+
+    private Transform _transform;
+    private Vector3 _startPosition;
+    private float _offset;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _startPosition = _transform.position;
+        
+        // This offsets the start point for the vertical bob effect.
+        _offset = Random.Range(-2f, 3f);
+        
+        // Rotate the coin by a random offset, this makes groups of coins look more natural.
+        _transform.Rotate(Random.Range(0f, 180f) * Vector3.up);
+    }
+
+    private void Update()
+    {
+        _transform.Rotate((Time.deltaTime * rotateSpeed) * Vector3.up);
+        _transform.position = _startPosition + (Mathf.Sin((Time.time + _offset) * bobSpeed) * bobAmplitude) * Vector3.up;
+    }
+}
